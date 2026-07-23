9b9f6aee51ae4ca43cd33669edcfaac7920865f3
Ratshl1 <bashattlais2@gmail.com>
2026-07-23 23:59:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const dragon = document.getElementById("dragon1");
 
-dragon.addEventListener("click", function(){
+dragon.addEventListener("click", function () {
 
-    document.getElementById("scene1").classList.add("hidden");
+document.getElementById("scene1").classList.add("hidden");
 
-    document.getElementById("scene2").classList.remove("hidden");
+document.getElementById("scene2").classList.remove("hidden");
 
 });
