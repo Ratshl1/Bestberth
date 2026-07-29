0f3f16a6c0621bffef72bde63f1d2029c90342bc
Ratshl1 <bashattlais2@gmail.com>
2026-07-29 17:43:48 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const dragon = document.getElementById("dragonHitbox");
-
-dragon.addEventListener("click", function () {
-
-document.getElementById("scene1").classList.add("hidden");
-
-document.getElementById("scene2").classList.remove("hidden");
-
-});
